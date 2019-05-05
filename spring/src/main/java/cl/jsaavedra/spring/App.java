8c6a2de4394446c0c8c799582d8ca1daf7cf0ea6
Jonathan Saavedra <jonathan.saavedra08@gmail.com>
2019-05-05 19:18:46 -0300

    Tutorial 10, uso colecciones en mapeo de beans.

--- spring/src/main/java/cl/jsaavedra/spring/App.java
+++ spring/src/main/java/cl/jsaavedra/spring/App.java
@@ -4,18 +4,29 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import cl.jsaavedra.beans.Ciudad;
 import cl.jsaavedra.beans.Persona;
 
 public class App {
 
 	public static void main(String[] args) {
-		
-		ApplicationContext  appContext = new ClassPathXmlApplicationContext("cl/jsaavedra/xml/beans.xml");
-				
+
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("cl/jsaavedra/xml/beans.xml");
+
 		Persona per = (Persona) appContext.getBean("personaBean2"); // o (Persona) appContext.getBean(Persona.class)
-		System.out.println(per.getId() + " " + per.getNombre() + " " + per.getApodo() + " " + per.getPais().getNombre() + " "+ per.getPais().getCiudad().getNombre()) ;
+
+		String nombreCiudades = "";
+
+		for (Ciudad ciudad : per.getPais().getCiudades()) {
+
+			nombreCiudades += ciudad.getNombre() + "-";
+
+		}
+
+		System.out.println(per.getId() + " " + per.getNombre() + " " + per.getApodo() + " " + per.getPais().getNombre()
+				+ " " + nombreCiudades);
 		
-		((ConfigurableApplicationContext)appContext).close();
+		((ConfigurableApplicationContext) appContext).close();
 
 	}
 
